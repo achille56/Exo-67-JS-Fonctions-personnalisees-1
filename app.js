a4efd64c076582abdf2e6892bb282b5126bab960
achille56 <leclercqjm59@gmail.com>
2022-11-21 20:32:29 +0100

    exo 67

---
+++ app.js
@@ -0,0 +1,13 @@
+function maFonction(paraId,newText) {
+    let p = document.getElementById(paraId);
+    p.innerHTML = newText;
+}
+
+maFonction("p1", "jean-mich");
+maFonction("p2","comment");
+maFonction("p3","tu");
+maFonction("p4","vas");
+maFonction("p5","?");
+
+
+
